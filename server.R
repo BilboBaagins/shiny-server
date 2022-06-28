b14df2f9ecded586595d2e18047ee72382f28ca4
Billy Archbold <billy.archbold@woodplc.com>
2022-06-28 01:12:51 +0100

    Updating the date format

--- server.R
+++ server.R
@@ -3855,7 +3855,7 @@ shinyServer(function(input, output, session) {
           "majorDate",
           "Date of Major",
           value = Sys.Date(), # Default to today's date.
-          format = "dd/mm/YYYY",
+          format = "dd/mm/yyyy",
           width = 350
         ),
         style = "margin-left: 15px;"
